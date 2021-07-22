8304132312ea02105073c0a9721649bc01a74cea
scarter31191 <scarter31191@gmaile.com>
2021-07-21 22:42:57 -0400

    uploaded logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import logo from '../Logo.png'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#"><img src={logo} alt="logo..." /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
+    
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
@@ -16,7 +18,6 @@ const Navbar = () => {
         <li className="nav-item">
           <a className="nav-link" href="#">Link</a>
         </li>
-        
       </ul>
       
     </div>
