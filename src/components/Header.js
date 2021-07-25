0b28cb46b6bb49d76992e81049cbe5b563c9fd5c
scarter31191 <scarter31191@gmaile.com>
2021-07-25 09:52:50 -0400

    added galaxy background

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Software Engineer & Creator</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
