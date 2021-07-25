43eeff3d62d5933bc417678adc282dc278bc7f40
scarter31191 <scarter31191@gmaile.com>
2021-07-25 10:02:06 -0400

    added typed and header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Typed from 'react-typed';
 
 const Header = () => {
     return (
         <div className="header-wraper">
             <div className="main-info">
                 <h1>Software Engineer & Creator</h1>
+                <Typed
+                    className='typed-text'
+                    strings={['Web Design', 'Web Dev', 'Mobile Desing', 'Mobile Dev', 'Web Dev Tutor' ]}
+                />
             </div>
         </div>
     )
