4ad90d02bd246de1b5f66bc309e6a47059787b45
scarter31191 <scarter31191@gmaile.com>
2021-07-25 23:37:42 -0400

    added particles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <>
+    <Particles
+    className='particles-canvas'
+      params={{
+        particles: {
+          number: {
+            value: 40,
+            density: {
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape: {
+            type: 'square',
+            stroke: {
+              width: 6,
+              color: '#f9ab00'
+            }
+          }
+        }
+      }}
+    />
       <Navbar />
       <Header />
     </>
