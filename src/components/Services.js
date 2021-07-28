1caed1b9d1c6b32afab85cfd4aba5ebce360694a
scarter31191 <scarter31191@gmaile.com>
2021-07-27 23:17:05 -0400

    added icon gem and put icons in Services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faChalkboard, faLaptopCode, faMobile, faPencilRuler } from '@fortawesome/free-solid-svg-icons'
 
 function Services() {
     return (
@@ -8,6 +11,7 @@ function Services() {
                     <div className='row'>
                         <div className='col-mid-3 col-sm-6'>
                             <div className='box'>
+                            <FontAwesomeIcon icon={faPencilRuler} size='4x'/>
                                 <h3>Web Design</h3>
                                 <p> I approach each project individually and always focus on the result.</p>
                             </div>
@@ -15,6 +19,7 @@ function Services() {
                         {/* - */}
                         <div className='col-mid-3 col-sm-6'>
                             <div className='box'>
+                            <FontAwesomeIcon icon={faLaptopCode} size='4x'/>
                                 <h3>Web Developer</h3>
                                 <p> Your website will be built with new and proven technologies</p>
                             </div>
@@ -22,6 +27,7 @@ function Services() {
                         {/* - */}
                         <div className='col-mid-3 col-sm-6'>
                             <div className='box'>
+                            <FontAwesomeIcon icon={faMobile} size='4x'/>
                                 <h3>Mobile Developer</h3>
                                 <p> Your apps will be built to your expectations and a user friendly flow</p>
                             </div>
@@ -29,6 +35,7 @@ function Services() {
                         {/* - */}
                         <div className='col-mid-3 col-sm-6'>
                             <div className='box'>
+                            <FontAwesomeIcon icon={faChalkboard} size='4x'/>
                                 <h3>Full-Stack Software Engineer Tutor</h3>
                                 <p> The goal is to provide value to students with industry best practices and the confidence to build their carries path </p>
                             </div>
