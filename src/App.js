a51689c4b5964a69ebc0b733cb9b11cf90eb764f
scarter31191 <scarter31191@gmaile.com>
2021-08-06 23:13:24 -0400

    add skills component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Particles from 'react-particles-js';
 import About from './components/AboutMe';
 import Services from './components/Services';
+import Skills from './components/Skills';
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
       <Header />
       <About />
       <Services />
+      <Skills />
     </>
   );
 }
