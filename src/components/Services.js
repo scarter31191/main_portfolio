a788d849f256455a335da417b1666b4880b577c5
scarter31191 <scarter31191@gmaile.com>
2021-08-06 21:26:26 -0400

    fixed services icon box

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,33 +9,33 @@ function Services() {
             <h1 className='py-5'>my services</h1>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-mid-3 col-sm-6'>
+                    <div className='col-md-3 col-sm-6'>
                         <div className='box'>
-                            <FontAwesomeIcon icon={faPencilRuler} size='4x'/>
+                            <FontAwesomeIcon icon={faPencilRuler} size='2x'/>
                                 <h3>Web Design</h3>
                                 <p> I approach each project individually and always focus on the result.</p>
                         </div>
                     </div>
                         {/* - */}
-                    <div className='col-mid-3 col-sm-6'>
+                    <div className='col-md-3 col-sm-6'>
                         <div className='box'>
-                            <FontAwesomeIcon icon={faLaptopCode} size='4x'/>
+                            <FontAwesomeIcon icon={faLaptopCode} size='2x'/>
                             <h3>Web Developer</h3>
                             <p> Your website will be built with new and proven technologies</p>
                         </div>
                     </div>
                         {/* - */}
-                    <div className='col-mid-3 col-sm-6'>
+                    <div className='col-md-3 col-sm-6'>
                         <div className='box'>
-                            <FontAwesomeIcon icon={faMobile} size='4x'/>
+                            <FontAwesomeIcon icon={faMobile} size='2x'/>
                             <h3>Mobile Developer</h3>
                             <p> Your apps will be built to your expectations and a user friendly flow</p>
                         </div>
                     </div>
                         {/* - */}
-                    <div className='col-mid-3 col-sm-6'>
+                    <div className='col-md-3 col-sm-6'>
                         <div className='box'>
-                            <FontAwesomeIcon icon={faChalkboard} size='4x'/>
+                            <FontAwesomeIcon icon={faChalkboard} size='2x'/>
                             <h3>Full-Stack Software Engineer Tutor</h3>
                             <p> The goal is to provide value to students with industry best practices and the confidence to build their carries path </p>
                         </div>
