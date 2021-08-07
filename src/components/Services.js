a9ff21e0b8eec7d09b800eb12454612e2fc1818c
scarter31191 <scarter31191@gmaile.com>
2021-08-06 21:07:26 -0400

    added services background

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,39 +6,38 @@ import { faChalkboard, faLaptopCode, faMobile, faPencilRuler } from '@fortawesom
 function Services() {
     return (
         <div className='services'>
-            <div className='py-5'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-mid-3 col-sm-6'>
-                            <div className='box'>
+            <h1 className='py-5'>my services</h1>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-mid-3 col-sm-6'>
+                        <div className='box'>
                             <FontAwesomeIcon icon={faPencilRuler} size='4x'/>
                                 <h3>Web Design</h3>
                                 <p> I approach each project individually and always focus on the result.</p>
-                            </div>
                         </div>
+                    </div>
                         {/* - */}
-                        <div className='col-mid-3 col-sm-6'>
-                            <div className='box'>
+                    <div className='col-mid-3 col-sm-6'>
+                        <div className='box'>
                             <FontAwesomeIcon icon={faLaptopCode} size='4x'/>
-                                <h3>Web Developer</h3>
-                                <p> Your website will be built with new and proven technologies</p>
-                            </div>
+                            <h3>Web Developer</h3>
+                            <p> Your website will be built with new and proven technologies</p>
                         </div>
+                    </div>
                         {/* - */}
-                        <div className='col-mid-3 col-sm-6'>
-                            <div className='box'>
+                    <div className='col-mid-3 col-sm-6'>
+                        <div className='box'>
                             <FontAwesomeIcon icon={faMobile} size='4x'/>
-                                <h3>Mobile Developer</h3>
-                                <p> Your apps will be built to your expectations and a user friendly flow</p>
-                            </div>
+                            <h3>Mobile Developer</h3>
+                            <p> Your apps will be built to your expectations and a user friendly flow</p>
                         </div>
+                    </div>
                         {/* - */}
-                        <div className='col-mid-3 col-sm-6'>
-                            <div className='box'>
+                    <div className='col-mid-3 col-sm-6'>
+                        <div className='box'>
                             <FontAwesomeIcon icon={faChalkboard} size='4x'/>
-                                <h3>Full-Stack Software Engineer Tutor</h3>
-                                <p> The goal is to provide value to students with industry best practices and the confidence to build their carries path </p>
-                            </div>
+                            <h3>Full-Stack Software Engineer Tutor</h3>
+                            <p> The goal is to provide value to students with industry best practices and the confidence to build their carries path </p>
                         </div>
                     </div>
                 </div>
